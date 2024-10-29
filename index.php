aafeedeef8c818fa5fadbc5c8b0622a7ce28105e
olti <sogojevaolti451@gmail.com>
2024-10-29 17:41:45 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,78 @@
+<?php 
+
+// $school = "Digital School";
+// echo " I love $school 2";
+
+
+// $x=5;
+// $y=10;
+
+// echo  $x + $y;
+// echo  $x - $y;
+// echo  $x / $y;
+// echo  $x * $y;
+// echo  $x % $y;
+
+
+
+// $a = "Digital";
+// $b ="School";
+
+// $c = $a . $b;
+// echo "$c \n";
+
+
+// $school2 = "School is not cool ";
+// echo str_replace("not", "very" ,$school2);
+
+//  $name = " Olti Sogoejva ";
+//  echo str_word_count(name);
+
+
+// $student = "Usame Mjekiqi";
+
+// echo strlen($student);
+
+
+// $n1 = "Olti Sogojeva";
+
+// echo strrev($n1);
+
+
+// $num = -5;
+
+// if($num < 0){
+//     echo " $num is less than 0"
+// }
+
+
+// $x = 1;
+
+// while($x <= 5){
+//     echo "the number is $x ";
+//     $x++;
+// }
+
+
+//  $x = 1;
+
+
+// do{
+//     echo "the number is $x <br> ";
+//     $x++;
+// }while($x <= 5);
+
+
+// for($x=0; $x <=10; $x++){
+//     echo"$x<br>";
+// }
+
+
+// $cars = array( "Audi", "bmw", "tesla");
+//     foreach ($cars as $values){
+//         echo "$values<br>";
+//     }
+
+
+
+?>
